04faf720ac0f0c9b126bb5ee455ce89cd403328c
Louis Fradin <louis.fradin@gmail.com>
2016-10-10 00:07:30 +0200

    Minor improvements

--- js/wall.js
+++ js/wall.js
@@ -14,14 +14,18 @@ Wall.prototype.displayMessage = function(id, previous, firstId){
   // Download message
   this.api.request("GET", "/messages/"+id, function(msg){
     var html = "<div class=\"message-text col-md-10 col-sm-9 col-xs-12\"><div>"+replaceSpecialChars(msg.text)+"</div></div>";
-    html += "<div class=\"message-infos col-md-2 col-sm-3 col-xs-12\">par <b>"+msg.name+"</b><br/>"+msg.time+"</div>";
+    html += "<div class=\"message-infos col-md-2 col-sm-3 col-xs-12\">";
+    html += "<span class=\"message-author\">par <b>"+msg.name+"</b></span><br/>";
+    html += "<span class=\"message-time\">à "+msg.time.substr(12,9)+"</span>";
+    html += "</div>";
     $("#message-"+id).empty().append(html);
 
     // === ADAPT TEXT SIZE
     // Get original size
     var fontSize = parseInt($("#message-"+id+" .message-text div").css('font-size'));
     // Adapt size until its too big
-    while( ($("#message-"+id+" .message-text div").width() < $("#message-"+id+" .message-text").width() && fontSize < 37)
+    while( ($("#message-"+id+" .message-text div").width() < $("#message-"+id+" .message-text").width()
+            && $("#message-"+id+" .message-text div").height() < $("#message-"+id+" .message-text").height())
             || fontSize < 17) {
         fontSize += 1;
         $("#message-"+id+" .message-text div").css('font-size', fontSize + "px" );
@@ -34,7 +38,7 @@ Wall.prototype.displayMessage = function(id, previous, firstId){
 
 Wall.prototype.updateStatus = function(){
   var d = new Date();
-  $("#update-status span").empty().append(
+  $("#update-bar .time").empty().append(
     d.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1")
   );
 };
